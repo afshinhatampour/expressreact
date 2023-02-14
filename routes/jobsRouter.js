81c3c4db651041493431a82132995b12d93bc758
afshin <afshin89hatampour@gmail.com>
2023-02-14 17:59:36 +0330

    forth commit

---
+++ routes/jobsRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {getAllJobs, updateJob, deleteJob, createJob, showStats} from "../controllers/jobsController.js";
+
+const router = express.Router();
+
+router.route('/').post(createJob).get(getAllJobs);
+router.route('/stats').get(showStats);
+router.route(':id').delete(deleteJob).patch(updateJob);
+export default router;
---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from "./db/connect.js";
+
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRouter.js";
+
+// middleware
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
